c22d85646809e8e86561af3adf63ab5e30919ec3
LAMBERMONT Jody <jody.lambermont@gmail.com>
2019-11-05 17:08:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,29 @@ function dataFetcher()
     var hashPasswordToHex = hashPassword.toString(CryptoJS.enc.Hex);
     console.log(hashPasswordToHex);
 
+    // Need first 5 from hashed password to send with api call for k-anonymity
     var shortHashPasswordHex = hashPasswordToHex.substring(0,5);
     console.log(shortHashPasswordHex);
 
+    // this sliced hash password is to get the corresponding object in the JSON answer (the first 5 are missing from the substring - K-anonymity)
     var slicedHashPasswordHex = hashPasswordToHex.slice(5);
     console.log(slicedHashPasswordHex);
 
+    // building the get request
+    // https://api.pwnedpasswords.com/range/
+    const https = new XMLHttpRequest;
+    const url = 'https://api.pwnedpasswords.com/range/' + slicedHashPasswordHex;
+    console.log(url);
+
+    https.open("get", url);
+    https.send();
+
+    https.onreadystatechange=function(){
+        if(this.readyState == 4 && this.status == 200)
+        {
+            console.log(https.responseText);
+        }
+    }
 
     return true;
 }
